c6efce069822c749bd92333e797b81a366c2fa25
kristiankyvik <kristianruizkyvik@gmail.com>
2020-10-19 10:18:37 +0200

    removed character

--- pages/banks/[bank_id]/index.js
+++ pages/banks/[bank_id]/index.js
@@ -94,7 +94,7 @@ export default function Index() {
                 </Link>
                 <span className="leading-relaxed text-gray-600 text-small">{account.currency}</span>
                 <div className="font-mono font-light text-gray-900 text-2xl leading-none mb-2 truncate pt-4">${account.balance.current}</div>
-              </div>å
+              </div>
               <svg className="cursor-pointer absolute right-0 bottom-0 h-5 w-5 m-10 text-gray-600 hover:cursor-pointer" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" onClick={() => { setSidePanelJSON(account); setSidePanelState(true); }}>
                 <path strokeLinecap="round" strokeLinejoin="round" strokeaWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
